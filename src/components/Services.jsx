51e19565910eb0639328875d73136e66d5f9972f
Ashutosh Anand <100065770+weedisneed@users.noreply.github.com>
2023-03-21 14:00:24 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import img1 from "../assets/3.png";
+import img2 from "../assets/4.png";
 const Services = () => {
-  return
+  return (
     <div>
-      Services
-    </div>
-  
-};
+      <Carousel
+        infiniteLoop
+        autoPlay
+        showStatus={false}
+        showArrows={false}
+        interval={1000}
+        showThumbs={false} //shows thumbnails below slide
+        // showIndicators={false}
+      >
 
-export default Services;
+        <div>
+          <img src={img1} alt="Item1"/>
+          <p className="legend">Full Stack</p>
+        </div>
+
+        <div>
+          <img src={img2} alt="Item1"/>
+          <p className="legend">Application Developer</p>
+        </div>
+
+
+      </Carousel>
+    </div>
+  )
+}
+
+export default Services
